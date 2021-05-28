b9d5c9385907800885c9a8282be3030e61a292bb
Adam Murray <adamm@cypress.io>
2021-05-28 17:02:56 -0500

    fix: remove package lock. add necessary packages. update angular.json. add ct spec. config files per install instructions.

---
+++ src/app/app.component.cy-spec.ts
@@ -0,0 +1,17 @@
+import { initEnv, mount } from '../../../cypress/npm/angular';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('shows the input', () => {
+    // Init Angular stuff
+    initEnv(AppComponent);
+    // You can also :
+    // initEnv({declarations: [AppComponent]});
+    // initEnv({imports: [MyModule]});
+
+    // component + any inputs object
+    mount(AppComponent, { title: 'World' });
+    // use any Cypress command afterwards
+    cy.contains('Welcome to World!');
+  });
+});
